b7a719b64042395b1a217d943da615ef8065b1a5
arsnovv <arsnov22@mail.ru>
2024-08-27 20:06:26 +0700

    simplified prime()

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,6 +3,17 @@ import math
 from brain_games.cli import welcome_user
 from brain_games.games.start_games import start_game
 
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
 
 def prime():
     name = welcome_user()
@@ -11,18 +22,6 @@ def prime():
     def get_question_and_answer():
         random_number = random.randint(1, 100)
         question = f'Question: {random_number}'
-
-        def is_prime(n):
-            if n <= 1:
-                return False
-            if n == 2:
-                return True
-            if n % 2 == 0:
-                return False
-            for i in range(3, int(math.sqrt(n)) + 1, 2):
-                if n % i == 0:
-                    return False
-            return True
         correct_answer = 'yes' if is_prime(random_number) else 'no'
         return question, correct_answer
 
